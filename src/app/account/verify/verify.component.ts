cd87cddf638632d49fb05a5cc64b15d63a1cb5b9
pocytamy <phamvantuan.133.1996@gmail.com>
2023-04-01 00:54:55 +0700

    verify done

--- src/app/account/verify/verify.component.ts
+++ src/app/account/verify/verify.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router } from '@angular/router';
-import { AccountService } from '../../service/account/account.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AccountService} from '../../service/account/account.service';
 
 @Component({
-  selector: 'app-verify',
-  templateUrl: './verify.component.html',
-  styleUrls: ['./verify.component.css']
+    selector: 'app-verify',
+    templateUrl: './verify.component.html',
+    styleUrls: ['./verify.component.css']
 })
-export class VerifyComponent implements OnInit{
-  check!:boolean;
-  code:string=this.route.snapshot.params['id'];
-  constructor(private router:Router,
-              private route: ActivatedRoute,
-              private accountService: AccountService) {
-  }
-  ngOnInit(){
+export class VerifyComponent implements OnInit {
+    check!: boolean;
+    code!: string;
 
-  }
-  verify(){
-    this.accountService.verify(this.code).subscribe(res=>{
-      if(res){
-        this.check=true;
-      }else {
-        this.check=false;
-      }
-    })
-  }
-  goToLogin(){
-    this.router.navigate(['/login'])
-  }
-  goToRegister(){
-    this.router.navigate(['/register'])
-  }
+    constructor(private router: Router,
+                private route: ActivatedRoute,
+                private accountService: AccountService) {
+    }
+
+    ngOnInit() {
+        this.code=this.route.snapshot.params['code'];
+        this.verify();
+    }
+
+    verify() {
+        this.accountService.verify(this.code).subscribe(res => {
+            if (res) {
+                this.check = true;
+            } else {
+                this.check = false;
+            }
+        })
+    }
+
+    goToLogin() {
+        this.router.navigate(['/login'])
+    }
+
+    goToRegister() {
+        this.router.navigate(['/register'])
+    }
 }
